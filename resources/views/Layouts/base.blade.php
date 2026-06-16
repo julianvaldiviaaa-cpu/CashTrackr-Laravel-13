00c1f778792095b50461ff099f674415f84e003f
JulianValdivia <julianvaldiviaaa@gmail.com>
2026-06-16 15:41:04 -0600

    fix: ruta correcta del manifest en blade layouts

--- resources/views/Layouts/base.blade.php
+++ resources/views/Layouts/base.blade.php
@@ -14,7 +14,7 @@
     @fonts
 
     <!-- Styles / Scripts -->
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+    @if (file_exists(public_path('build/.vite/manifest.json')) || file_exists(public_path('hot')))
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
 
